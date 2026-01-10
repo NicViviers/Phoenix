68fa9db9c0499d99b14511a1716fb2ddfa3ef4b6
NicViviers <nicviviers4@gmail.com>
2026-01-10 20:38:34 +0200

    Bug fixes, implicit extensions support, PATH var support with base dirs

--- src/engine.rs
+++ src/engine.rs
@@ -1,10 +1,24 @@
 use ariadne::{Label, Report, ReportKind, Source};
 use crate::ast::{Module, Program, Spanned, StreamStrategy};
-use std::{fs::File, process::{Command, Stdio}};
+use std::{env, fs::File, io::{Error, ErrorKind}, path::PathBuf, process::{Command, Stdio}};
 use std::collections::HashMap;
+
+
+#[cfg(target_os = "windows")]
+mod PLATFORM_VARS {
+    pub const PATH_SEPARATOR: char = ';';
+    pub const BASE_DIR: &'static str = "USERPROFILE";
+}
+
+#[cfg(target_os = "linux")]
+mod PLATFORM_VARS {
+    const PATH_SEPARATOR: char = ':';
+    const BASE_DIR: &'static str = "HOME";
+}
 
 pub struct Engine {
     pub cur_dir: String, // TODO: Implement paths
+    path: Vec<String>,
     vars: Vec<String>, // TODO: Implement environment variables. Load from Windows / bashrc ?
     builtins: HashMap<&'static str, builtins::BuiltinFn>,
     source: String
@@ -12,8 +26,17 @@ pub struct Engine {
 
 impl Engine {
     pub fn new() -> Self {
+        let path = env::var_os("PATH")
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .split(PLATFORM_VARS::PATH_SEPARATOR)
+            .map(|p| p.to_string())
+            .collect();
+
         Self {
-            cur_dir: String::new(),
+            cur_dir: Engine::get_base_dir(),
+            path,
             vars: Vec::new(),
             builtins: builtins::builtin_registry(),
             source: String::new()
@@ -64,7 +87,8 @@ impl Engine {
                 return Ok(())
             }
 
-            let mut cmd = Command::new(&source[stmt.value.program]);
+            let executable = self.find_executable(&source[stmt.value.program])?;
+            let mut cmd = Command::new(executable);
             cmd.args(stmt.value.argv.iter().map(|arg| &source[arg.clone()]));
 
             let stdin = match prev_stdout.take() {
@@ -117,7 +141,8 @@ impl Engine {
             return builtin(self, &stmt);
         }
 
-        let mut cmd = Command::new(&source[stmt.value.program]);
+        let executable = self.find_executable(&source[stmt.value.program])?;
+        let mut cmd = Command::new(executable);
         cmd.args(stmt.value.argv.iter().map(|arg| &source[arg.clone()]));
 
         match stmt.value.stdin {
@@ -144,12 +169,41 @@ impl Engine {
 
         Ok(())
     }
+
+    fn find_executable(&self, cmd: &str) -> std::io::Result<PathBuf> {
+        let extensions = if cfg!(windows) {
+            vec!["exe", "cmd", "bat", "com"]
+        } else {
+            vec![""]
+        };
+
+        for dir in &self.path {
+            for ext in &extensions {
+                let mut full_path = PathBuf::from(dir).join(cmd);
+
+                if !ext.is_empty() {
+                    full_path.set_extension(ext);
+                }
+
+                if full_path.exists() && full_path.is_file() {
+                    return Ok(full_path)
+                }
+            }
+        }
+
+        // TODO: Can we generate a ariadne error somehow?
+        Err(Error::new(ErrorKind::NotFound, format!("Unrecognized command '{}'", cmd)))
+    }
+
+    fn get_base_dir() -> String {
+        env::var_os(PLATFORM_VARS::BASE_DIR).unwrap().into_string().unwrap()
+    }
 }
 
 // TODO: Finish implementing builtins module
 mod builtins {
-    use std::{collections::HashMap, io::{Read, Write}};
-    use crate::ast::{Spanned, Program};
+    use std::{collections::HashMap, env, io::{Read, Write}};
+    use crate::{ast::{Program, Spanned}, engine::Engine};
 
     pub type BuiltinFn = fn(&mut crate::Engine, &Spanned<Program>) -> std::io::Result<()>;
 
@@ -166,12 +220,16 @@ mod builtins {
     fn cd(engine: &mut crate::Engine, stmt: &Spanned<Program>) -> std::io::Result<()> {
         // TODO: Implement 'cd' command with no argv that should go back to home directory
         // TODO: Implement implicit relative paths such as 'C:\>cd Users' currently moves to 'Users\>' which doesn't exist
+        // TODO: Lexer crashes with no token implementation of 'cd ..\'
         if let Some(path) = stmt.value.argv.get(0) {
-            // TODO: Double check this code
             let path = str::from_utf8(&engine.source.as_bytes()[path.clone()]).unwrap();
 
-            std::env::set_current_dir(path)?;
+            env::set_current_dir(path)?;
             engine.cur_dir = path.to_string();
+        } else {
+            let path = Engine::get_base_dir();
+            env::set_current_dir(&path)?;
+            engine.cur_dir = path;
         }
 
         Ok(())
@@ -193,7 +251,7 @@ mod builtins {
             println!("{}", content);
         } else {
             // TODO: We don't support piping for internals
-            // could we possibly change that to support piping *to* internals and not *from*?
+            // could we possibly change that to support piping *to* internals at least
             let mut buffer = Vec::new();
             std::io::stdin().read_to_end(&mut buffer)?;
             println!("{}", String::from_utf8(buffer).unwrap());
